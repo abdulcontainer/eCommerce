09b149f11397c894b15526398c4828f0e40a3fb2
abdulContainer <mailtoa.wahid@gmail.com>
2022-01-05 23:31:04 +0500

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
-Route::view('/','login');
+Route::view('/login','login');
+Route::post('/login',[UserController::class,'login'])->name('login');
+
+Route::get('/',[ProductController::class,'index'])->name('/');
